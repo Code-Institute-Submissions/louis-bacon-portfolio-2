c4eb32607e4dbcf25136632615ac018e10309b7e
lbacon17 <louis.bacon17@icloud.com>
2020-10-06 18:41:50 +0000

    added for loop to disable input of empty spaces in form input fields

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// back to top button appears when scrolling from top of page
+
 $(document).ready(function() {
     bttt = document.getElementById("returnToTop");
             
@@ -12,7 +14,21 @@ $(document).ready(function() {
     };
 });
 
+// takes user back to top of page when clicking on button
+
 function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+var inputField = document.querySelectorAll("input");
+
+// disables spaces in all input fields in forms
+for (let i = 0; i < inputField.length; i++) {
+    inputField[i].addEventListener("keypress", function() {
+        var key = event.keyCode;
+        if (key === 32) {
+            event.preventDefault();
+        }
+    });
 }
